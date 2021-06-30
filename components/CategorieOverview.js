eb13b108a4e0843cb816eebf29b9e877bbf107a4
LeBi26 <leon.biermann@gmx.net>
2021-06-30 21:46:38 +0200

    added style to categorie overview

--- components/CategorieOverview.js
+++ components/CategorieOverview.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { View, Button, Text } from "react-native";
+import { View, TouchableHighlight, Text } from "react-native";
 import styles from "./styles.js";
 
 const CategorieOverview = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Wähle eine Kategorie:</Text>
-      <Button
-        title={"Motivation"}
+    <View style={[styles.margin, styles.flexContainer]}>
+      <Text style={[styles.secondaryText, styles.center, styles.marginBottom]}>
+        Wähle eine Kategorie:
+      </Text>
+      <TouchableHighlight
+        style={[styles.buttonPrimary]}
         onPress={() => props.navigation.navigate("Categorie")}
-      />
-      <Button
-        title={"Inspiration"}
+      >
+        <Text style={styles.primaryButtonText}>Motivation</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        style={[styles.buttonPrimary]}
         onPress={() => props.navigation.navigate("Categorie")}
-      />
-      <Button
-        title={"Klassiker"}
+      >
+        <Text style={styles.primaryButtonText}>Inspiration</Text>
+      </TouchableHighlight>
+      <TouchableHighlight
+        style={[styles.buttonPrimary]}
         onPress={() => props.navigation.navigate("Categorie")}
-      />
+      >
+        <Text style={styles.primaryButtonText}>Klassiker</Text>
+      </TouchableHighlight>
     </View>
   );
 };
