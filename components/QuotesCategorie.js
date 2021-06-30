8e7dcb74a061f8b32fd52376e80812697d66da29
LeBi26 <leon.biermann@gmx.net>
2021-06-30 21:53:56 +0200

    added some style to the quotes

--- components/QuotesCategorie.js
+++ components/QuotesCategorie.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Button, Image } from "react-native";
+import { View, TouchableHighlight, Text } from "react-native";
 import Constants from "expo-constants";
 import styles from "./styles.js";
 
@@ -9,16 +9,17 @@ import Quotes from "../assets/Quotes.js";
 const QuotesCategorie = () => {
   const [count, setCount] = useState(0);
   return (
-    <View style={styles.container}>
+    <View style={[styles.margin, styles.flexContainer, styles.spaceAround]}>
       <Zitat {...Quotes[count]} />
-
-      <Button
-        title="Nächstes Zitat"
+      <TouchableHighlight
+        style={styles.buttonPrimary}
         onPress={() => {
           if (count + 1 === Quotes.length) setCount(0);
           else setCount(count + 1);
         }}
-      />
+      >
+        <Text style={styles.primaryButtonText}>Nächstes Zitat</Text>
+      </TouchableHighlight>
     </View>
   );
 };
--- components/Zitat.js
+++ components/Zitat.js
@@ -13,9 +13,12 @@ const selectImages = (name) => {
 const Zitat = (props) => {
   const selectImage = selectImages;
   return (
-    <View style={styles.flex}>
-      <Image style={styles.image} source={selectImage(props.from)} />
-      <View style={styles.container}>
+    <View style={[styles.flexContainer]}>
+      <Image
+        style={[styles.image, styles.marginBottom]}
+        source={selectImage(props.from)}
+      />
+      <View>
         <Text style={styles.quote}>"{props.quote}"</Text>
         <Text style={styles.quoteAppend}>Zitat von: {props.from}</Text>
       </View>
