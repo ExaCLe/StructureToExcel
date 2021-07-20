df9bbe119a9c76aea54261724028f63b2cda4068
LeBi26 <leon.biermann@gmx.net>
2021-07-20 16:53:32 +0200

    added the button for updating the goal progress

--- components/Goal.js
+++ components/Goal.js
@@ -3,38 +3,80 @@ import { View, Text, TouchableHighlight } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import styles from "./styles.js";
 import * as colors from "./../assets/colors.js";
-
-const Goal = (props) => {
-  return (
-    <View>
-      <TouchableHighlight
-        style={styles.habitContainer}
-        onPress={() =>
-          props.navigation.navigate("GoalsDetails", { ...props.goal })
-        }
-      >
-        <View style={styles.container2}>
-          <View style={styles.containerHorizontal}>
-            <Ionicons
-              name={props.goal.icon}
-              size={25}
-              color={colors.PrimaryTextColor}
-              style={styles.padding}
-            />
-            <Text
-              style={[
-                styles.normalText,
-                styles.primaryTextColor,
-                styles.padding,
-              ]}
-            >
-              {props.goal.name}
-            </Text>
+import * as SQLite from "expo-sqlite";
+const db = SQLite.openDatabase("goals.db");
+class Goal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...this.props.goal };
+  }
+  handleFullfilled = () => {
+    this.setState(
+      (prevState) => ({ progress: prevState.progress + 1 }),
+      db.transaction((tx) => {
+        tx.executeSql(
+          "UPDATE goals SET progress = ? WHERE id = ? ",
+          [this.state.progress, this.state.id],
+          () => {},
+          () => {
+            console.log("error");
+          }
+        );
+      })
+    );
+  };
+  render() {
+    return (
+      <View>
+        <TouchableHighlight
+          style={styles.habitContainer}
+          onPress={() =>
+            this.props.navigation.navigate("GoalsDetails", {
+              ...this.state,
+            })
+          }
+        >
+          <View style={styles.container2}>
+            <View style={styles.containerHorizontal}>
+              <Ionicons
+                name={this.state.icon}
+                size={25}
+                color={colors.PrimaryTextColor}
+                style={styles.padding}
+              />
+              <Text
+                style={[
+                  styles.normalText,
+                  styles.primaryTextColor,
+                  styles.padding,
+                ]}
+              >
+                {this.state.name}
+              </Text>
+            </View>
+            <View style={styles.containerHorizontal}>
+              <Text style={styles.primaryTextColor}>
+                {this.state.progress + " / " + this.state.repetitions}
+              </Text>
+              <TouchableHighlight
+                onPress={() => {
+                  this.handleFullfilled();
+                }}
+                underlayColor="transparent"
+              >
+                <Ionicons
+                  name="checkmark-circle-outline"
+                  size={25}
+                  color={colors.PrimaryTextColor}
+                  style={styles.padding}
+                />
+              </TouchableHighlight>
+            </View>
           </View>
-        </View>
-      </TouchableHighlight>
-    </View>
-  );
-};
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
 
 export default Goal;
