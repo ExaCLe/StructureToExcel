aaaf454073bab29bd22eb805367bdd846a52b56b
LeBi26 <leon.biermann@gmx.net>
2021-08-12 09:49:20 +0200

    changed standard servers

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import React from "react";
 import App2 from "./screens/App.js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Parse from "parse/react-native";
+import { Platform } from "react-native";
 
 Parse.setAsyncStorage(AsyncStorage);
 
-Parse.initialize(
-  "sy6KefxN8AKgzYK8ZU4Rtn7uPQ2MjDRyJ7C8qrvz",
-  "YT4CUinkgxjaxC86bjrvjF6CuFANQw0Z7LtIr5zu"
-);
-Parse.serverURL = "https://parseapi.back4app.com/";
-// Parse.initialize("myAppId");
-// Parse.serverURL = "https://test-parse-server-for-app.herokuapp.com/parse";
+if (Platform.OS === "android") {
+  Parse.initialize(
+    "sy6KefxN8AKgzYK8ZU4Rtn7uPQ2MjDRyJ7C8qrvz",
+    "YT4CUinkgxjaxC86bjrvjF6CuFANQw0Z7LtIr5zu"
+  );
+  Parse.serverURL = "https://parseapi.back4app.com/";
+} else {
+  Parse.initialize("myAppId");
+  Parse.serverURL = "https://test-parse-server-for-app.herokuapp.com/parse";
+}
 
 export default class App extends React.Component {
   constructor(props) {
